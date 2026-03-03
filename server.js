a8588af81fc7cee1f946d134a18c15c049dce496
Nagesh Dhanashetti <nagesh.dhanashetti@outlook.com>
2026-03-03 22:42:10 +0530

    feat: integrate Express server and update package dependencies
    - Added Express as a dependency to the project.
    - Updated server.js to use Express application from app.js instead of a simple HTTP server.
    - Configured the server to listen on a specified port using Express.
    - Updated package.json to include necessary Angular CLI and compiler dependencies.

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const http = require('http');
+// earlier app was started with only server.js by using the command node server.js,
+// but now we have app.js which is the main file for our express application, 
+// so we need to import that file here in server.js
+// we are importing the app.js file which is exporting the express application,
+// so we can use that app variable to create our server and listen on the specified port
 
-const server = http.createServer((req, res) => {
-  res.end('This is my first response!');
-});
+// We will start the server using the command node server.js, 
+// and it will run the express application defined in app.js
+const app = require('./backend/app');
 
-server.listen(process.env.PORT || 3000, () => {
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
   console.log('Server running at http://localhost:3000/');
 });
